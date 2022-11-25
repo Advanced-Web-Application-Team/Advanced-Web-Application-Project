27a5ecdb6b090407d93dd85bf706c14a7fcf05dd
unknown <muikku.jere@hotmail.com>
2022-11-25 15:33:22 +0200

    Added labels to correct sectors and changed color of the graph a bit for the sub sectors

--- Product/Ad_web_app_project-master/Website/frontend/src/components/AllChartDesigns/DoughnutChartOfV9.jsx
+++ Product/Ad_web_app_project-master/Website/frontend/src/components/AllChartDesigns/DoughnutChartOfV9.jsx
@@ -39,11 +39,12 @@ function DoughnutChartOfV9() {
       { 
         callbacks: 
         {
-         footer: (context) => 
-         {
-          return subSectorTextReturn(context[0].label);
-         },
-         afterFooter: () => {return "Click to show/hide sub-sectors"}
+        label: (context) => {
+          return `${context.dataset.labels[context.dataIndex]}: ${context.dataset.data[context.dataIndex]}%`
+        },
+        afterFooter: () => {
+          return "Click to show/hide sub-sectors"
+        }
 
         },
       },
@@ -56,21 +57,26 @@ function DoughnutChartOfV9() {
 };
    
 
-  
+  //Get and fetch data from the database
 
     let {allDataOfV9, fetchAllDataOfV9} = useContext(ChartContext); 
     useEffect(() => {
         fetchAllDataOfV9();
     },[]);
-    //Get main sectors  from the database
+   
+    //Get main sectors from the database
      let mainSector = allDataOfV9.map((data) => data.sector);
-
+     //Get sub sectors from database
+     let subSector = allDataOfV9.map((data) => data.sub_sector)
     //Energy total greenhouse gas submissions
     let energyTotal = allDataOfV9.filter(x => x.sector === "Energy").map(x => x.Share_of_global_greenhouse_gas_emissions).reduce((acc, element) => acc += element, 0);
+  
     // Industry total greenhouse gas submissions
     let industryTotal = allDataOfV9.filter(x => x.sector === "Industry ").map(x => x.Share_of_global_greenhouse_gas_emissions).reduce((acc, element) => acc += element, 0);
+   
     //Agriculture, Forestry & Land Use total greenhouse gas submissions
     let agricultureTotal = parseFloat(allDataOfV9.filter(x => x.sector === "Agriculture, Forestry & Land Use").map(x => x.Share_of_global_greenhouse_gas_emissions).reduce((acc, element) => acc += element, 0).toFixed(1));
+    
     //Waste total greenhouse gas submissions
     let wasteTotal = allDataOfV9.filter(x => x.sector === "Waste").map(x => x.Share_of_global_greenhouse_gas_emissions).reduce((acc, element) => acc += element, 0);
     
@@ -136,16 +142,23 @@ function DoughnutChartOfV9() {
       return text;
     };
     
-    let testDataArray = [energyTotal,industryTotal, agricultureTotal, wasteTotal];
-    let testArray =  [2,3,4,5,6,4]
-  
+  // Filter to filter duplicate values off arrays
     let Filter = (arr) =>
     {
      let result = arr.filter((element, i) => arr.indexOf(element) === i)
 
      return result;
     };
-
+  // Function for randomized colors
+    let randomColors = () => 
+    {
+      let r = Math.floor(Math.random()*255)
+      var g = Math.floor(Math.random()*255)
+      var b = Math.floor(Math.random()*255)
+    
+    return "rgb("+r+","+g+","+b+")";
+    
+    }
      // Get Array Values of C02 by each sector 
      const getValuesOfSubSector = (id) => {
 
@@ -213,10 +226,8 @@ function DoughnutChartOfV9() {
   };
 
   //Get Array Labels by each sub-sub sector
-
   const getLabelsOfSubSubSector = (id) => {
     let getSectorName = "";
-
     switch (id) {
       case 0:
         getSectorName = "Energy";
@@ -235,7 +246,6 @@ function DoughnutChartOfV9() {
     };
 
     let arrayLabels = allDataOfV9.filter(x => x.sector === getSectorName).map(x => x.sub_sub_sector);
-
     return arrayLabels;
 
   };
@@ -246,7 +256,6 @@ function DoughnutChartOfV9() {
    const getLabelsOfSubSector = (id) => {
 
     let getSectorName = "";
-
     switch (id) {
       case 0:
         getSectorName = "Energy";
@@ -270,41 +279,61 @@ function DoughnutChartOfV9() {
   
     return removeDuplicate;
   };
-
-
-  
-  
+  const getColor = (id) => 
+  {
+    let getSectorName = "";
+    let color = ""
+    switch (id) {
+      case 0:
+        getSectorName = "Energy";
+        color = "rgb(255,255,0)"
+        break;
+      case 1: 
+        getSectorName = "Industry ";
+        color ="rgb(169,169,169)"
+        break;
+      case 2:
+        getSectorName = "Agriculture, Forestry & Land Use";
+        color = "rgb(0,100,0)"
+        break;
+      case 3:
+        getSectorName = "Waste"
+        color = "rgb(139,69,19)"
+        break;
+      default:
+        break;
+    }
+    return color;
+  }
+// Sector data values
+  let sectorData = [energyTotal,industryTotal, agricultureTotal, wasteTotal];
       const data = {
          labels: Filter(mainSector),
           datasets:
               [{
-                  data: testDataArray,
+                  data: sectorData,
                   borderColor: "rgb(0, 0, 0)",
                   backgroundColor: ["rgb(255,255,0)","rgb(169,169,169)","rgb(0,100,0)","rgb(139,69,19)"],
                   labels: Filter(mainSector),
               },
               {
                 data: getValuesOfSubSector(getNumber),
-                borderColor: "rgb(0, 0, 0)",
-                backgroundColor: ["rgb(255,255,0)","rgb(169,169,169)","rgb(0,100,0)","rgb(139,69,19)"],
+                borderColor: [randomColors()],
+                backgroundColor: getColor(getNumber),
                 labels: getLabelsOfSubSector(getNumber),
                 hidden: showHidden,
             },
             {
-              label: "SubSubSector",
               data: getValuesOfSubSubSector(getNumber),
-              borderColor: "rgb(0, 0, 0)",
-              backgroundColor: ["rgb(255,255,0)","rgb(169,169,169)","rgb(0,100,0)","rgb(139,69,19)"],
+              borderColor: randomColors(),
+              backgroundColor: getColor(getNumber),
               labels: getLabelsOfSubSubSector(getNumber),
               hidden: showHidden,
           },
-
-            
-           
+          
             ]
             
       }
-
   return (
     <div>
         <div style={{width: '80%', margin: "auto", border: "3px solid black", borderRadius: 4, padding: 10, backgroundColor: "white", alignItems: "center", justifyContent: "center"}}>
