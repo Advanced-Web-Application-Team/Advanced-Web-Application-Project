8d08420c1172b8813f52152ba9f940d8d50b3b31
thongdang2708 <duythong2708@gmail.com>
2022-11-30 00:28:00 +0200

    Add and delete layout

--- Product/Ad_web_app_project-master/Website/frontend/src/store/actions/LayoutAddActions.js
+++ Product/Ad_web_app_project-master/Website/frontend/src/store/actions/LayoutAddActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {url} from "../../context/LineChartContext"
-const API_URL = "/layout/";
+const API_URL = process.env.REACT_APP_SERVER_URL + "/layout/";
 
 //Add layout
 
@@ -16,7 +16,7 @@ export const addLayout = (layoutInputs) => async (dispatch, getState) => {
             }
         };
 
-        let check = await axios.post(url+API_URL, layoutInputs, config);
+        let check = await axios.post(API_URL, layoutInputs, config);
 
         
         dispatch({
@@ -52,4 +52,70 @@ export const removeLocalStorageOfLayout = () => async (dispatch, getState) => {
         type: "REMOVE_ALL"
     });
     localStorage.removeItem("layout");
+};
+
+//Fetch a list of charts
+
+export const fetchLayouts = () => async (dispatch, getState) => {
+
+    try {
+
+    let token = getState().auth.user.token;
+
+    let config = {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    };
+
+    let response = await axios.get(API_URL + "allByOne", config);
+
+    dispatch({
+        type: "FETCH_LAYOUTS",
+        payload: response.data
+    });
+
+    } catch (error) {
+        let message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+   
+        dispatch({
+            type: "FETCH_LAYOUT_FAIL",
+            payload: message
+        })
+
+    }
+
+};
+
+//Delete layout by user
+
+export const deleteLayouts = (id) => async (dispatch, getState) => {
+
+    try {
+    
+    let token = getState().auth.user.token;
+    
+    let config = {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    };
+
+    let response = await axios.delete(API_URL + `delete/${id}`, config);
+    let deleteLayout = response.data;
+    dispatch({
+        type: "DELETE_LAYOUTS_SUCCESSFULLY",
+        payload: deleteLayout
+    });
+
+
+    } catch (error) {
+        let message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+   
+        dispatch({
+            type: "DELETE_LAYOUTS_FAIL",
+            payload: message
+        })
+    }
+
 };
--- Product/Ad_web_app_project-master/Website/frontend/src/store/reducers/LayoutAddReducer.js
+++ Product/Ad_web_app_project-master/Website/frontend/src/store/reducers/LayoutAddReducer.js
@@ -1,8 +1,30 @@
 
-export const layoutAddReducer = (state = {isSuccess: false, isError: false, message: ""}, action) => {
+export const layoutAddReducer = (state = {layoutArrays: [], isSuccess: false, isError: false, message: ""}, action) => {
 
     switch (action.type) {
 
+        case "FETCH_LAYOUTS":
+            return {
+                ...state,
+                layoutArrays: action.payload
+            }
+        case "FETCH_LAYOUTS_FAIL":
+            return {
+                ...state,
+                isError: true,
+                message: action.payload
+            }
+        case "DELETE_LAYOUTS_SUCCESSFULLY":
+            return {
+                ...state,
+                layoutArrays: state.layoutArrays.filter((layout) => layout._id !== action.payload._id)
+            }
+            case "DELETE_LAYOUTS_FAIL":
+                return {
+                    ...state,
+                    isError: true,
+                    message: action.payload
+                }
         case "ADD_LAYOUT_SUCCESSFULLY":
             return {
                 ...state,
