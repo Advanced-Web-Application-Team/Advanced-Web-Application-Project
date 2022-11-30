b3fa2d9161928e6a308b320ee605ba5595976a69
thongdang2708 <duythong2708@gmail.com>
2022-11-30 17:43:48 +0200

    Done with delete user

--- Product/Ad_web_app_project-master/Website/frontend/src/components/screens/MainPage/UserProfilePage.jsx
+++ Product/Ad_web_app_project-master/Website/frontend/src/components/screens/MainPage/UserProfilePage.jsx
@@ -4,7 +4,9 @@ import { fetchLayouts } from '../../../store/actions/LayoutAddActions';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import SingleLayoutDisplayInProfile from '../../SingleItem/SingleLayoutDisplayInProfile';
-
+import { deleteUser } from '../../../store/actions/UserActions';
+import {toast} from "react-toastify"
+import { useNavigate } from 'react-router-dom';
 
 //User Page for log-in user 
 function UserProfilePage() {
@@ -26,14 +28,32 @@ function UserProfilePage() {
         dispatch(fetchLayouts());
       },[]);
       
-    console.log(layoutArrays);
+       //Set navigate
+
+       let navigate = useNavigate();
+    
+      
+       //Handle delete user
+   
+       const handleDelete = () => {
+           if (window.confirm("Are you sure to delete your own account? All created layouts are also deleted!")) {
+             dispatch(deleteUser());
+             navigate("/");
+             toast.success("Delete yourself successfully!");
+           } else {
+             console.log("No delete!");
+           }
+       };
     
 
   return (
         <div>
         <div className='container mx-auto p-5'>
-            <h1 className='text-black text-3xl font-bold my-5'> Profile Page: {user.username}</h1>
+        <div className="my-10 flex justify-between items-center">
+            <h1 className='text-black text-3xl font-bold my-5' onClick={handleDelete}> Profile Page: {user.username}</h1>
 
+            <div className='btn btn-lg bg-emerald-400 focus:outline-0' onClick={handleDelete}> Delete User </div>
+            </div>
             <div className='bg-gray-100 w-10/12 xl:w-6/12 lg:w-6/12 md:w-6/12 border-2 rounded-lg border-black p-10 shadow-lg'>
                
                     <h3 className='text-black font-bold text-xl'> Name: {user.username}</h3>
