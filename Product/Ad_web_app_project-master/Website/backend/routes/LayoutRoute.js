d8f77169efde03147a0d76eb207de0e6605dd743
thongdang2708 <duythong2708@gmail.com>
2022-11-30 03:40:44 +0200

    Show specific view visualization page created by user

--- Product/Ad_web_app_project-master/Website/backend/routes/LayoutRoute.js
+++ Product/Ad_web_app_project-master/Website/backend/routes/LayoutRoute.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
 const router = express.Router();
-const {addLayout, getAllLayoutByUser, deleteLayout} = require("../controllers/LayoutControllers");
+const {addLayout, getAllLayoutByUser, deleteLayout ,getSingleLayout, getAllLayouts} = require("../controllers/LayoutControllers");
 const {protect} = require("../middleware/verifyToken");
 
 router.route("/").post(protect, addLayout);
+router.route("/all").get(getAllLayouts);
+router.route("/single/:layoutId").get(getSingleLayout);
 router.route("/allByOne").get(protect, getAllLayoutByUser);
 router.route("/delete/:layoutId").delete(protect, deleteLayout);
 
--- Product/Ad_web_app_project-master/Website/frontend/src/store/store.js
+++ Product/Ad_web_app_project-master/Website/frontend/src/store/store.js
@@ -4,6 +4,7 @@ import {createStore, applyMiddleware, combineReducers} from "redux";
 import thunk from "redux-thunk";
 import { layoutReducer } from "./reducers/LayoutForDesignPageReducer";
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { layoutDisplayReducer } from "./reducers/LayoutDisplayReducer";
 import { layoutAddReducer } from "./reducers/LayoutAddReducer";
 import { authReducer } from "./reducers/AuthReducers";
 
@@ -13,7 +14,8 @@ let middleware = [thunk];
 let rootReducer = combineReducers({
     auth: authReducer,
     layout: layoutReducer,
-    layoutAdd: layoutAddReducer
+    layoutAdd: layoutAddReducer,
+    layoutContent: layoutDisplayReducer
 });
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
